4af6a06d4abe0b7b1305b60e131f93dc53ea0524
unknown <67815810+unknown-01-hacker@users.noreply.github.com>
2021-02-23 20:39:12 +0530

    Update __main__.py

--- miakhalifa/__main__.py
+++ miakhalifa/__main__.py
@@ -179,8 +179,8 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="❤️Help",callback_data="help_back"),InlineKeyboardButton(text="Creator💛",url="https://t.me/USER_IS_HACKER")]]
-    keyboard += [[InlineKeyboardButton(text="➕Connect Group", callback_data="main_connect"),InlineKeyboardButton(text="Add Me ➕",url="t.me/{}?startgroup=true".format(bot.username))]]
+    keyboard = [[InlineKeyboardButton(text="🤝 Help",callback_data="help_back"),InlineKeyboardButton(text="My Owner 🇮🇳",url="https://t.me/USER_IS_HACKER")]]
+    keyboard += [[InlineKeyboardButton(text="🌐 Connect Group", callback_data="main_connect"),InlineKeyboardButton(text="our group ",url="t.me/{}?startgroup=true".format(bot.username))]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
