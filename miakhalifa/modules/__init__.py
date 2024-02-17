3af37d53ea6b3e9a22cb3d8503f9af5376f314cb
Brav0 <68178115+jishnu10@users.noreply.github.com>
2024-02-17 15:51:56 +0530

    Update __init__.py

--- miakhalifa/modules/__init__.py
+++ miakhalifa/modules/__init__.py
@@ -1,9 +1,12 @@
-from miakhalifa import LOAD, NO_LOAD, LOGGER
+import logging
+from os.path import dirname, basename, isfile
+import glob
 
+LOAD = []
+NO_LOAD = []
+LOGGER = logging.getLogger(__name__)
 
 def __list_all_modules():
-    from os.path import dirname, basename, isfile
-    import glob
     # This generates a list of modules in this folder for the * in __main__ to work.
     mod_paths = glob.glob(dirname(__file__) + "/*.py")
     all_modules = [basename(f)[:-3] for f in mod_paths if isfile(f)
@@ -11,23 +14,17 @@ def __list_all_modules():
                    and not f.endswith('__init__.py')]
 
     if LOAD or NO_LOAD:
-        to_load = LOAD
-        if to_load:
-            if not all(any(mod == module_name for module_name in all_modules) for mod in to_load):
-                LOGGER.error("Invalid loadorder names. Quitting.")
-                quit(1)
+        to_load = LOAD if LOAD else all_modules
 
-        else:
-            to_load = all_modules
+        if not all(mod in all_modules for mod in to_load):
+            LOGGER.error("Invalid load order names. Quitting.")
+            quit(1)
 
         if NO_LOAD:
-            LOGGER.info("Not loading: {}".format(NO_LOAD))
+            LOGGER.info("Not loading: %s", NO_LOAD)
             return [item for item in to_load if item not in NO_LOAD]
 
-        return to_load
-
     return all_modules
-
 
 ALL_MODULES = sorted(__list_all_modules())
 LOGGER.info("Modules to load: %s", str(ALL_MODULES))
